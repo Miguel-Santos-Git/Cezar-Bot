c50e15d9f13ebd96c38a9e6992224c3e2549fe21
Miguel Santos <miguelitodobem7@gmail.com>
2026-08-23 22:16:11 -0300

    Start a create bot
    I want write more things, but i need sleep.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Tomorrow i start, haha")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(".",intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot iniciou com sucesso")
+
+@bot.command()
+async def Hello(ctx:commands.Context):
+    await ctx.reply("Hello, how are you?")
+
+bot.run("")
