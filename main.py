4b7edcae1bfe0aac395627337f4269e8e66fa5f1
Miguel Santos <miguelitodobem7@gmail.com>
2026-08-24 14:07:29 -0300

    Create bot basic structure
    I add token.json for save the token, and add some slash commands.

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import discord
 from discord.ext import commands
+import json
 
 intents = discord.Intents.all()
-bot = commands.Bot(".",intents=intents)
+bot = commands.Bot(command_prefix=".",intents=intents)
+
+with open("token.json","r") as file:
+    token = json.load(file)["token"]
 
 @bot.event
 async def on_ready():
-    print("Bot iniciou com sucesso")
+    sings = await bot.tree.sync()
+    print(f"{len(sings)} comandos sincronizados.")
+    print("Bot iniciou com sucesso.")
 
-@bot.command()
-async def Hello(ctx:commands.Context):
-    await ctx.reply("Hello, how are you?")
+@bot.tree.command(description="Say Hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.display_name}! How are you?")
 
-bot.run("")
+
+bot.run(token)
