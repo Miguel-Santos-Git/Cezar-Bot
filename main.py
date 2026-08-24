f3a39423f923e606d96fa32264c5dba3a5417ffc
Miguel Santos <miguelitodobem7@gmail.com>
2026-08-24 19:01:34 -0300

    I add get_infos and change some parts.
    In title

--- main.py
+++ main.py
@@ -32,20 +32,20 @@ def already_have_char(username):
 async def on_ready():
     sings = await bot.tree.sync()
     print(f"{len(sings)} comandos sincronizados.")
-    print("Bot iniciou com sucesso.")
+    print("Bot start with sucess.")
 
-@bot.tree.command(description="Create user info",)
+@bot.tree.command(description = "Create user info",)
 async def create_character(interaction: discord.Interaction, name:str):
     if not name:
-        await interaction.response.send_message(f"You don't select a character name.")
+        await interaction.response.send_message(f"You don't select a character name.",ephemeral = True)
         return
     name_exist = char_name_exist(name)
     if name_exist:
-        await interaction.response.send_message(f"This name already in use.")
+        await interaction.response.send_message(f"This name already in use.",ephemeral = True)
         return
     already_char = already_have_char(interaction.user.name)
     if already_char:
-        await interaction.response.send_message(f"You already have a character.")
+        await interaction.response.send_message(f"You already have a character.",ephemeral = True)
 
     cursor.execute("""
     INSERT INTO main_table (username, name, level, money)
@@ -53,7 +53,23 @@ async def create_character(interaction: discord.Interaction, name:str):
                     """,(interaction.user.name, name))
 
     connection.commit()
-    await interaction.response.send_message(f"Your RP character is create with sucess.")
+    await interaction.response.send_message(f"Your RP character is create with sucess.",ephemeral = True)
+
+@bot.tree.command(description = "Get your character info")
+async def get_infos(interaction: discord.Interaction):
+    have_char = already_have_char(interaction.user.name)
+    if not have_char:
+        await interaction.response.send_message(f"You dont have a character. Use '/create_character character_name'",ephemeral = True)
+
+    cursor.execute("SELECT * FROM main_table WHERE username = ?",(interaction.user.name,))
+    id, username, name_char, level, money = cursor.fetchone()
+
+    await interaction.response.send_message(f"""Your infos: 
+    Character Name: {name_char}
+    Level: {level}
+    Money: {money}
+    
+    """,ephemeral = True) 
 
 connection.commit()
 bot.run(token)
